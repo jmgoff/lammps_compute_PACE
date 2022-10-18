db9a31b652ff1a63ed61d69a4b3849b327545a47
ares201005 <ares201005@gmail.com>
2022-10-18 11:09:03 -0600

    update build script

--- build_pace_latteqeq.sh
+++ build_pace_latteqeq.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-LATTEQEQPATH=/vast/home/zhy/ecp/anders-code/QEQ_lammps_Yu/
+#LATTEQEQPATH=/path/to/latteqeq/
+LATTEQEQPATH=/vast/home/zhy/ecp/LATTE-git
 
  cmake  -DCMAKE_BUILD_TYPE=Release  -DPKG_LATTEQEQ=ON  -DCMAKE_PREFIX_PATH="$LATTEQEQPATH/install" -DPKG_ML-PACE=on -D PKG_MOLECULE=on -DCMAKE_CXX_STANDARD=11   ../cmake
- #cmake  -DCMAKE_BUILD_TYPE=Release  -DPKG_LATTEQEQ=ON  -DCMAKE_PREFIX_PATH="$LATTEQEQPATH/install" -DPKG_ML-PACE=on -D PKG_MOLECULE=on  -D BUILD_MPI=on  -DCMAKE_CXX_STANDARD=11   ../cmake
 
  #copy lammps-user-pace package 
  cp -r ../tmp/lammps-user-pace-v.2022.09.27/ML-PACE/ace-evaluator/* lammps-user-pace-v.2022.09.27/ML-PACE/ace-evaluator/
